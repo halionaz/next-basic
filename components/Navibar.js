46f33043d173f13d62075088d2ce44f0155e4c72
halionaz <samhsj@naver.com>
2022-04-27 06:46:51 -0400

    Learning styled jsx

--- components/Navibar.js
+++ components/Navibar.js
@@ -7,11 +7,19 @@ export default function Navibar(){
     return (
         <nav>
             <Link href="/">
-                <a style={{"color" : router.pathname === "/" ? "red" : "blue"}}>Home</a>
+                <a className={router.pathname === "/" ? "active" : ""}>Home</a>
             </Link>
             <Link href="/about">
-                <a style={{"color" : router.pathname === "/about" ? "red" : "blue"}}>About</a>
+                <a className={ router.pathname === "/about" ? "active" : ""}>About</a>
             </Link>
+            <style jsx>{`
+                a {
+                    text-decoration: none;
+                }
+                .active {
+                    color : tomato;
+                }
+            `}</style>
         </nav>
     )
 }
