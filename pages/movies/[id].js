79e5d753f13a87478afa2b603bad79245f3b1c77
halionaz <samhsj@naver.com>
2022-05-04 23:44:20 -0400

    오류 수정

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -4,6 +4,7 @@
 
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import Seo from "../../components/Seo";
 
 export default function Detail() {
     const router = useRouter();
@@ -11,15 +12,22 @@ export default function Detail() {
 
     useEffect(() => {
         (async () => {
-            const result = await (
-                await fetch(`/api/movies/${router.query.id}`)
-            ).json();
-            setDetail(result.overview);
+            if(Object.keys(router.query).length){
+                // 처음에는 Router이 작동하지 않기에, query가 비어있음
+                // 이런 경우 fetch를 하면 오류가 나므로, 스킵함.
+                // JS가 실행된 이후엔 router.query가 업데이트 되므로
+                // 그때 useEffect가 다시 실행되면서 Detail을 불러옴
+                const result = await (
+                    await fetch(`/api/movies/${router.query.id}`)
+                ).json();
+                setDetail(result.overview);
+            }
         })();
-    }, []);
+    }, [router.query]);
 
     return (
         <div>
+            <Seo title={router.query.title}></Seo>
             <h4>{router.query.title}</h4>
             <img src={router.query.img}></img>
             {detail}
